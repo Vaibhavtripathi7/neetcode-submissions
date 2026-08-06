ea25a890deb3f3a5ddd090273b093e52f37211a8
Vaibhav Tripathi <149421336+Vaibhavtripathi7@users.noreply.github.com>
2026-08-06 23:54:06 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        def maxrob(i,nums,cache):
+            if len(nums) == 1: return nums[0]
+            if i >= len(nums): return 0 #d base case
+            if i in cache: return cache[i]
+            result = max(nums[i] + (maxrob(i+2, nums, cache)), maxrob(i+1, nums,cache))
+            cache[i] = result
+            return result
+        # i am overcomplicating the problem : maybe 
+        cache1 = {}
+        result1 = maxrob(0, nums[1:], cache1)
+        cache2 = {}
+        result2 = maxrob(0, nums[:-1], cache2)
+        return max(result1, result2)
+        # return max(maxrob)
+        # return maxrob
