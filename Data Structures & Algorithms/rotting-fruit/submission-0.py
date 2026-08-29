6cf36cfd11bebd04a30fcfcf1de40b53ee967edf
Vaibhav Tripathi <149421336+Vaibhavtripathi7@users.noreply.github.com>
2026-08-29 18:38:23 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        queue = deque()
+        visited = set()
+        fresh_count = 0
+        minutes = 0  
+
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 2: 
+                    queue.append((i,j)) # that's the start point: for multi source BFS
+                elif grid[i][j] == 1: 
+                    fresh_count += 1 
+
+        directions = [[0,1],[0,-1],[-1,0],[1,0]] # all directions: 
+
+        # now the count: var --we increment it when -- we pass one level 
+        while queue:
+            level_nodes = len(queue) 
+            for _ in range(level_nodes):
+                r, c = queue.popleft()
+
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc # all four directions 
+                    # one by one append in queue too : 
+                    # now we check the cell : 
+                    if nr < 0 or nc < 0 or nr >= row or nc >= col or (nr,nc) in visited or grid[nr][nc] != 1:
+                        continue
+                    visited.add((nr,nc))
+                    fresh_count -= 1 
+                    queue.append((nr,nc))
+            minutes += 1
+        return -1 if fresh_count > 0 else minutes - 1 
